cba39f36dfd882f72d2b57d6f8fba3638229ca64
xristospk <patrick.krybus@daimler.com>
2021-10-01 02:43:12 +0200

    Update Startup.cs

--- Startup.cs
+++ Startup.cs
@@ -22,9 +22,9 @@ namespace AzureToolkit {
         public void ConfigureServices(IServiceCollection services) {
             services.AddMvc();
 
-            string user = "atpk";
-            string pw = "Norkimo12!";
-            var connection = $"Server=tcp:azuretoolkit-pk.database.windows.net,1433;Initial Catalog=azuretoolkit;Persist Security Info=False;User ID={user};Password={pw};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+            string user = "user";
+            string pw = "password";
+            var connection = $"Server=tcp:{DATABSE}.database.windows.net,1433;Initial Catalog=azuretoolkit;Persist Security Info=False;User ID={user};Password={pw};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
             services.AddDbContext<AzureToolkitContext>(options => options.UseSqlServer(connection));
         }
 
